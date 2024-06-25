5255265447dfa99d3412f7220edcf216ffd0e8d6
Francis B M <francisfr007@gmail.com>
2024-06-25 11:58:36 +0530

    Added files of Basic Nteflix_App frontend

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+import './App.css'
+import {action,orginals} from './url'
+
+function App() {
+  return (
+    <div className="App">
+       <NavBar/>
+       <Banner/>
+       <RowPost url={orginals} title='Netflix Orginals'/>
+       <RowPost url={action} title='Action' isSmall />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = "8045cd893b6a2eac35da371cb1d936e0"
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
